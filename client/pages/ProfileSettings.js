10e44d47891be6f8aff43517b1d32e5591c142c0
omsurase <om.surase21@vit.edu>
2022-11-13 09:57:40 +0530

    changed widgets file name

--- client/pages/ProfileSettings.js
+++ client/pages/ProfileSettings.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
 import Settings from "../components/Settings";
@@ -6,7 +7,7 @@ import Navbar from "../components/Navbar";
 
 const styles = {
   wrapper:
-    "flex justify-center items-center h-screen w-screen bg-[#0f172a] text-white overflow-x-hidden",
+    "flex   justify-center items-center h-screen w-screen bg-[#0f172a] text-white overflow-x-hidden",
   columns:
     "flex justify-between h-screen w-full text-center  text-white gap-0.5",
   sides: "basis-1/4 bg-slate-900 h-full sticky top-5",
