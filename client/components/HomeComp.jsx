d3eb32cf71059501aa4cf0c169286e760b55da09
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2022-11-17 23:59:03 +0530

    enhance: responsiveness to home screen

--- client/components/HomeComp.jsx
+++ client/components/HomeComp.jsx
@@ -9,10 +9,10 @@ const styles = {
     "flex justify-center items-center h-screen w-screen bg-[#0f172a] text-white overflow-x-hidden",
   columns:
     "flex justify-between h-screen w-full text-center  text-white gap-0.5",
-  sides: "basis-1/4 bg-slate-900 h-full overflow-y-auto",
-  side2: "basis-3/4 bg-slate-900 h-full flex flex-row overflow-x-hidden  ",
-  feed: "basis-2/3 bg-slate-800 h-full overflow-x-hidden overflow-y-auto px-4",
-  widgets: "basis-1/3 bg-slate-900 h-full overflow-x-hidden overflow-y-auto",
+  sides: "hidden lg:inline lg:basis-1/4 bg-slate-900 h-full overflow-y-auto",
+  side2: "lg:basis-3/4 bg-slate-900 h-full flex flex-row overflow-x-hidden  ",
+  feed: "lg:basis-2/3 bg-slate-800 h-full overflow-x-hidden overflow-y-auto px-4",
+  widgets: "hidden lg:inline lg:basis-1/3 bg-slate-900 h-full overflow-x-hidden overflow-y-auto",
 };
 
 const HomeComp = () => {
---
+++ client/pages/walletTransfer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Widgets from '../components/Widgets'
+
+const WalletTransfer = () => {
+  return (
+    <div className='sm:inline hidden'>
+        <Widgets />
+    </div>
+  )
+}
+
+export default WalletTransfer
